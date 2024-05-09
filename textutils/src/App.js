83bb46354d163025d1b2612634fc752876543eae
leonischu <leonischu10@gmail.com>
2024-05-09 22:09:07 +0545

    Dark mode

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -2,15 +2,15 @@
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
-// import Textform from './components/Textform';
+import Textform from './components/Textform';
 
 function App() {
   return (
     <> 
 <Navbar title="Textutils"/>
 <div className="container">
-  {/* <Textform heading="Enter the text to analyze"/> */}
-  <About/>
+  <Textform heading="Enter the text to analyze"/>
+  {/* <About/> */}
 </div>
 
     </>
