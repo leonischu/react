7ec12a4d896b472778caa6880e4c032dd869e7f6
leonischu <leonischu10@gmail.com>
2024-05-10 21:18:35 +0545

    cwh

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,13 +1,15 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 
 function App() {
+  const [mode,setMode] = useState('light');//whether dark mode is enabled or not 
   return (
     <> 
-<Navbar title="Textutils"/>
+<Navbar title="Textutils" mode={mode}/>
 <div className="container">
   <Textform heading="Enter the text to analyze"/>
   {/* <About/> */}
