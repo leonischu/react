deda7e9eca3b531f8c1244f4fe6cfe7c61c0c89d
leonischu <leonischu10@gmail.com>
2024-05-11 21:05:39 +0545

    apna

---
+++ apna/apna_cart/src/components/navbar.js
@@ -0,0 +1,17 @@
+ import React from 'react'
+ 
+ class Navbar extends React.Component {
+
+    render() { 
+        return (<nav className="navbar  navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a classNameName="navbar-brand" >Apna Cart</a>
+          
+        
+          </div>
+        
+      </nav>
+    )}
+ }
+  
+ export default Navbar;
