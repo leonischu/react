f30a833a70fa3766e91d2f2ad80b285729cfb64b
leonischu <leonischu10@gmail.com>
2024-05-22 20:58:11 +0545

    new

---
+++ new/contact-app/src/components/AddContact.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const AddContact = () => {
+ return(
+    <div className='ui main'>
+        <h2>Add Contact </h2>
+        <form action="" className="ui form">
+            <div className="field">
+                <label >Name</label>
+                <input type="text" name='Name' placeholder='name' />
+            </div>
+            <div className="field">
+                <label >Email</label>
+                <input type="text" name='Email' placeholder='Email' />
+            </div>
+        </form>
+    </div>
+ );
+}
+
+export default AddContact;
---
+++ new/contact-app/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () =>{
+    return (
+    <div className="ui fixed menu">
+        <h2>Contact Manager</h2>
+    </div>);
+}
+export default Header;
